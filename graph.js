ed82b36908186e38525151403a6b94a4184dc2ed
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-31 11:12:00 -0500

    Initial graph set up complete

---
+++ graph.js
@@ -0,0 +1,33 @@
+const dims = { height: 500, width: 1100 };
+
+const svg = d3
+  .select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 100)
+  .attr('height', dims.height + 100);
+
+const graph = svg.append('g').attr('transform', 'translate(50,50)');
+
+// for data and firestore connection
+var data = [];
+
+db.collection('aemployees').onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+});
